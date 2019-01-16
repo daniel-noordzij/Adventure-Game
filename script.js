667ffac1230d2b1f942b1a4d3c4c37e386abd284
Daniel <smokeystreams@gmail.com>
2019-01-17 00:54:10 +0100

    movement, z-index depth illusion, animations

--- script.js
+++ script.js
@@ -30,10 +30,12 @@ function startGame() {
 	var using = false;
 	var menuOpen = false;
 	var movementSpeed = 4;
+	var stopAnim = false;
 	var pressedKeys = [];
 	var player = document.getElementById('player'),
 	style = window.getComputedStyle(player);
 	var slashTime = 5000;
+	var indexes = {0:"1", 4:"2", 8:"3", 12:"4", 16:"5", 20:"6", 24:"7", 28:"8", 32:"9", 36:"10", 40:"11", 44:"12", 48:"13", 52:"14", 56:"15", 60:"16", 64:"17", 68:"18", 72:"19", 76:"20", 80:"21", 84:"22", 88:"23", 92:"24", 96:"25", 100:"26", 104:"27", 108:"28", 112:"29", 116:"30", 120:"31"};
 
 
 	$(document.body).keydown(function (evt) {
@@ -60,29 +62,105 @@ function startGame() {
 		}
 	});
 
-	function getPosition(elem) {
-	 	var pos, width, height;
-		pos = $(elem).position();
-		width = $(elem).width();
-		height = $(elem).height();
-		return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+	function walkAnimation(direction) {
+		if (direction === upCode && pressedKeys[upCode] === true && stopAnim === false) {
+			stopAnim = true;
+			player.src = "images/player/p_up_static.png";
+			if (pressedKeys[upCode] === true) {
+				player.src = "images/player/p_up_walk1.png";
+				setTimeout(function() {
+					if (pressedKeys[upCode] === true) {
+						player.src = "images/player/p_up_walk2.png";
+						setTimeout(function() {
+							if (pressedKeys[upCode] === true) {
+								player.src = "images/player/p_up_walk3.png";
+								setTimeout(function() {
+									if (pressedKeys[upCode] === true) {
+										player.src = "images/player/p_up_walk4.png";
+										setTimeout(function(){
+											stopAnim = false;
+										}, 250);
+									}
+								}, 250);
+							}
+						}, 250);
+					}
+				}, 250);
+			}
+		} else if (direction === downCode && pressedKeys[downCode] === true && stopAnim === false) {
+			stopAnim = true;
+			player.src = "images/player/p_down_static.png";
+			if (pressedKeys[downCode] === true) {
+				player.src = "images/player/p_down_walk1.png";
+				setTimeout(function() {
+					if (pressedKeys[downCode] === true) {
+						player.src = "images/player/p_down_walk2.png";
+						setTimeout(function() {
+							if (pressedKeys[downCode] === true) {
+								player.src = "images/player/p_down_walk3.png";
+								setTimeout(function() {
+									if (pressedKeys[downCode] === true) {
+										player.src = "images/player/p_down_walk4.png";
+										setTimeout(function(){
+											stopAnim = false;
+										}, 250);
+									}
+								}, 250);
+							}
+						}, 250);
+					}
+				}, 250);
+			}
+		} else if (direction === leftCode && pressedKeys[leftCode] === true && stopAnim === false) {
+			stopAnim = true;
+			player.src = "images/player/p_left_static.png";
+			if (pressedKeys[leftCode] === true) {
+				player.src = "images/player/p_left_walk1.png";
+				setTimeout(function() {
+					if (pressedKeys[leftCode] === true) {
+						player.src = "images/player/p_left_walk2.png";
+						setTimeout(function() {
+							if (pressedKeys[leftCode] === true) {
+								player.src = "images/player/p_left_walk1.png";
+								setTimeout(function() {
+									if (pressedKeys[leftCode] === true) {
+										player.src = "images/player/p_left_walk3.png";
+										setTimeout(function(){
+											stopAnim = false;
+										}, 250);
+									}
+								}, 250);
+							}
+						}, 250);
+					}
+				}, 250);
+			}
+		} else if (direction === rightCode && pressedKeys[rightCode] === true && stopAnim === false) {
+			stopAnim = true;
+			player.src = "images/player/p_right_static.png";
+			if (pressedKeys[rightCode] === true) {
+				player.src = "images/player/p_right_walk1.png";
+				setTimeout(function() {
+					if (pressedKeys[rightCode] === true) {
+						player.src = "images/player/p_right_walk2.png";
+						setTimeout(function() {
+							if (pressedKeys[rightCode] === true) {
+								player.src = "images/player/p_right_walk1.png";
+								setTimeout(function() {
+									if (pressedKeys[rightCode] === true) {
+										player.src = "images/player/p_right_walk3.png";
+										setTimeout(function(){
+											stopAnim = false;
+										}, 250);
+									}
+								}, 250);
+							}
+						}, 250);
+					}
+				}, 250);
+			}
+		}
 	}
-
-	function comparePositions(p1, p2) {
-		var r1, r2;
-		r1 = p1[0] < p2[0] ? p1 : p2;
-		r2 = p1[0] < p2[0] ? p2 : p1;
-		return r1[1] > r2[0] || r1[0] === r2[0];
-	}
-
-	function getOverlap(a, b) {
-		var pos1 = getPosition(a),
-			pos2 = getPosition(b);
-		return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-	}
-
-	// var test = getOverlap();
-
 
 	setInterval(function() {
 		if(pressedKeys[upCode] === true && menuOpen === false) {
@@ -93,19 +171,19 @@ function startGame() {
 				newTop = newTopNum + "px";
 					
 				player.style.marginTop = newTop;
+				walkAnimation(upCode);
 			}
-				
 		}
 		if(pressedKeys[rightCode] === true && menuOpen === false) {
 			var side = style.getPropertyValue('margin-left');
 
-			if (side !== "712px") {
+			if (side !== "720px") {
 				var newSideNum = parseInt(side) + movementSpeed,
 				newSide = newSideNum + "px";
 					
 				player.style.marginLeft = newSide;
+				walkAnimation(rightCode);
 			}
-				
 		}
 		if(pressedKeys[leftCode] === true && menuOpen === false) {
 			var side = style.getPropertyValue('margin-left');
@@ -115,29 +193,35 @@ function startGame() {
 				newSide = newSideNum + "px";
 					
 				player.style.marginLeft = newSide;
+				walkAnimation(leftCode);
 			}
-				
 		}
 		if(pressedKeys[downCode] === true && menuOpen === false) {
 			var top = style.getPropertyValue('margin-top');
 
-			if (top !== "456px") {
+			if (top !== "436px") {
 				var newTopNum = parseInt(top) + movementSpeed,
 				newTop = newTopNum + "px";
 					
 				player.style.marginTop = newTop;
+				walkAnimation(downCode);
 			}
-				
 		}
 		if(pressedKeys[slashCode] === true) {
 			if(slashing === false && menuOpen === false) {
 					slashing = true;
-					player.src = "images/playerSlash.png";
+					/*player.src = "images/playerSlash.png";*/
 					setTimeout(function(){
-						player.src = "images/player.png";
+						/*player.src = "images/player.png";*/
 					}, 150);
 			}
 		}
+		// change between z-indexes to create depth illusion
+
+		var currentHeight = player.style.marginTop;
+		var indexNum = indexes[currentHeight];
+		player.style.zIndex = indexNum;
+		
 	}, 30);
 
 	$(document.body).keyup(function (evt) {
@@ -152,6 +236,19 @@ function startGame() {
 		}
 		if (evt.keyCode === useCode) {
 			using = false;
+		}
+		if (evt.keyCode === upCode) {
+			stopAnim = false;
+			player.src = "images/player/p_up_static.png";
+		} else if (evt.keyCode === downCode) {
+			stopAnim = false;
+			player.src = "images/player/p_down_static.png";
+		} else if (evt.keyCode === leftCode) {
+			stopAnim = false;
+			player.src = "images/player/p_left_static.png";
+		} else if (evt.keyCode === rightCode) {
+			stopAnim = false;
+			player.src = "images/player/p_right_static.png";
 		}
 	});
 }
