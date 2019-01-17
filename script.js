b7ea04374246367e4222e2296c4e00d239c8d9e3
Daniel Noordzij <smokeystreams@gmail.com>
2019-01-17 12:26:07 +0100

    collision detection semi-working, environment stuff added

--- script.js
+++ script.js
@@ -31,12 +31,16 @@ function startGame() {
 	var menuOpen = false;
 	var movementSpeed = 4;
 	var stopAnim = false;
+	var ableToMoveUp = true,
+		ableToMoveDown = true,
+		ableToMoveLeft = true,
+		ableToMoveRight = true;
 	var pressedKeys = [];
 	var player = document.getElementById('player'),
 	style = window.getComputedStyle(player);
 	var slashTime = 5000;
-	var indexes = {0:"1", 4:"2", 8:"3", 12:"4", 16:"5", 20:"6", 24:"7", 28:"8", 32:"9", 36:"10", 40:"11", 44:"12", 48:"13", 52:"14", 56:"15", 60:"16", 64:"17", 68:"18", 72:"19", 76:"20", 80:"21", 84:"22", 88:"23", 92:"24", 96:"25", 100:"26", 104:"27", 108:"28", 112:"29", 116:"30", 120:"31"};
-
+	var indexes = {0:"1", 4:"2", 8:"3", 12:"4", 16:"5", 20:"6", 24:"7", 28:"8", 32:"9", 36:"10", 40:"11", 44:"12", 48:"13", 52:"14", 56:"15", 60:"16", 64:"17", 68:"18", 72:"19", 76:"20", 80:"21", 84:"22", 88:"23", 92:"24", 96:"25", 100:"26", 104:"27", 108:"28", 112:"29", 116:"30", 120:"31", 124:"32", 128:"33", 132:"34", 136:"35", 140:"36", 144:"37", 148:"38", 152:"39", 156:"40", 160:"41", 164:"42", 168:"43", 172:"44", 176:"45", 180:"46", 184:"47", 188:"48", 192:"49", 196:"50", 200:"51", 204:"52", 208:"53", 212:"54", 216:"55", 220:"56", 224:"57", 228:"58", 232:"59", 236:"60", 240:"61", 244:"62", 248:"63", 252:"64", 256:"65", 260:"66", 264:"67", 268:"68", 272:"69", 276:"70", 280:"71", 284:"72", 288:"73", 292:"74", 296:"75", 300:"76", 304:"77", 308:"78", 312:"79", 316:"80", 320:"81", 324:"82", 328:"83", 332:"84", 336:"85", 340:"86", 344:"87", 348:"88", 352:"89", 356:"90", 360:"91", 364:"92", 368:"93", 372:"94", 376:"95", 380:"96", 384:"97", 388:"98", 392:"99", 396:"100", 400:"101", 404:"102", 408:"103", 412:"104", 416:"105", 420:"106", 424:"107", 428:"108", 432:"109", 436:"110"};
+	var hitboxes = document.getElementsByClassName("hitbox");
 
 	$(document.body).keydown(function (evt) {
 		if(!pressedKeys.includes(evt.keyCode)){
@@ -79,13 +83,13 @@ function startGame() {
 										player.src = "images/player/p_up_walk4.png";
 										setTimeout(function(){
 											stopAnim = false;
-										}, 250);
+										}, 200);
 									}
-								}, 250);
+								}, 200);
 							}
-						}, 250);
+						}, 200);
 					}
-				}, 250);
+				}, 200);
 			}
 		} else if (direction === downCode && pressedKeys[downCode] === true && stopAnim === false) {
 			stopAnim = true;
@@ -103,13 +107,13 @@ function startGame() {
 										player.src = "images/player/p_down_walk4.png";
 										setTimeout(function(){
 											stopAnim = false;
-										}, 250);
+										}, 200);
 									}
-								}, 250);
+								}, 200);
 							}
-						}, 250);
+						}, 200);
 					}
-				}, 250);
+				}, 200);
 			}
 		} else if (direction === leftCode && pressedKeys[leftCode] === true && stopAnim === false) {
 			stopAnim = true;
@@ -127,13 +131,13 @@ function startGame() {
 										player.src = "images/player/p_left_walk3.png";
 										setTimeout(function(){
 											stopAnim = false;
-										}, 250);
+										}, 200);
 									}
-								}, 250);
+								}, 200);
 							}
-						}, 250);
+						}, 200);
 					}
-				}, 250);
+				}, 200);
 			}
 		} else if (direction === rightCode && pressedKeys[rightCode] === true && stopAnim === false) {
 			stopAnim = true;
@@ -151,13 +155,13 @@ function startGame() {
 										player.src = "images/player/p_right_walk3.png";
 										setTimeout(function(){
 											stopAnim = false;
-										}, 250);
+										}, 200);
 									}
-								}, 250);
+								}, 200);
 							}
-						}, 250);
+						}, 200);
 					}
-				}, 250);
+				}, 200);
 			}
 		}
 	}
@@ -166,7 +170,7 @@ function startGame() {
 		if(pressedKeys[upCode] === true && menuOpen === false) {
 			var top = style.getPropertyValue('margin-top');
 
-			if (top !== "0px") {
+			if (top !== "0px" && ableToMoveUp === true) {
 				var newTopNum = parseInt(top) - movementSpeed,
 				newTop = newTopNum + "px";
 					
@@ -177,7 +181,7 @@ function startGame() {
 		if(pressedKeys[rightCode] === true && menuOpen === false) {
 			var side = style.getPropertyValue('margin-left');
 
-			if (side !== "720px") {
+			if (side !== "720px" && ableToMoveRight === true) {
 				var newSideNum = parseInt(side) + movementSpeed,
 				newSide = newSideNum + "px";
 					
@@ -188,7 +192,7 @@ function startGame() {
 		if(pressedKeys[leftCode] === true && menuOpen === false) {
 			var side = style.getPropertyValue('margin-left');
 
-			if (side !== "0px") {
+			if (side !== "0px" && ableToMoveLeft === true) {
 				var newSideNum = parseInt(side) - movementSpeed,
 				newSide = newSideNum + "px";
 					
@@ -199,7 +203,7 @@ function startGame() {
 		if(pressedKeys[downCode] === true && menuOpen === false) {
 			var top = style.getPropertyValue('margin-top');
 
-			if (top !== "436px") {
+			if (top !== "436px" && ableToMoveDown === true) {
 				var newTopNum = parseInt(top) + movementSpeed,
 				newTop = newTopNum + "px";
 					
@@ -219,10 +223,36 @@ function startGame() {
 		// change between z-indexes to create depth illusion
 
 		var currentHeight = player.style.marginTop;
-		var indexNum = indexes[currentHeight];
+		var indexNum = indexes[parseInt(currentHeight)];
 		player.style.zIndex = indexNum;
+
+		// Stop moving when hitting a hitbox
+		
+		var playerHitbox = player.getBoundingClientRect();
+
+		for (var i = hitboxes.length - 1; i >= 0; i--) {
+			var hitboxCoords = hitboxes[i].getBoundingClientRect();
+			var hitboxCoordsleft = parseInt(hitboxCoords.left) - 36;
+			var hitboxCoordsright = parseInt(hitboxCoords.right) + 36;
+/*			var hitboxCoordstop = parseInt(hitboxCoords.top) + 36;
+			var hitboxCoordsbottom = parseInt(hitboxCoords.bottom) + 36;*/
+			if (playerHitbox.top <= hitboxCoords.bottom && playerHitbox.left >= hitboxCoordsleft && playerHitbox.right <= hitboxCoordsright) {
+				ableToMoveUp = false;
+			} else {
+				ableToMoveUp = true;
+			}
+			if (playerHitbox.bottom >= hitboxCoords.top && playerHitbox.left >= hitboxCoordsleft && playerHitbox.right <= hitboxCoordsright) {
+				ableToMoveDown = false;
+			} else {
+				ableToMoveDown = true;
+			}
+		}
+
 		
 	}, 30);
+
+	/*var treeHitbox = hitboxes.getBoundingClientRect();
+	console.log(treeHitbox);*/
 
 	$(document.body).keyup(function (evt) {
 		pressedKeys[evt.keyCode] = false;
