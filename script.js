95db18686a4ae5a160558a59a3cad2d90ddfcf10
Daniel Noordzij <smokeystreams@gmail.com>
2019-01-22 11:57:36 +0100

    added menubar, saving & loading

--- script.js
+++ script.js
@@ -20,17 +20,70 @@ function saveControls() {
 	slashCode = document.getElementById("slashInput").value.toUpperCase().charCodeAt(0);
 }
 
-function startGame() {
-	document.getElementById('gameBorder').style.display = "inline-block";
+function controls() {
+	document.getElementById('controls').style.display = "inline-block";
 	document.getElementById('startButton').style.display = "none";
+	document.getElementById('contButton').style.display = "none";
+	document.getElementById('backButton').style.display = "inline-block";
+	document.getElementById('controlsButton').style.display = "none";
+}
+
+function backToMenu() {
 	document.getElementById('controls').style.display = "none";
+	document.getElementById('startButton').style.display = "inline-block";
+	document.getElementById('contButton').style.display = "inline-block";
+	document.getElementById('backButton').style.display = "none";
+	document.getElementById('controlsButton').style.display = "inline-block";
+}
+
+function contGame() {
+	document.getElementById('loadfile').style.display = "inline-block";
+	document.getElementById('startButton').style.display = "none";
+	document.getElementById('contButton').style.display = "none";
+	document.getElementById('backButton').style.display = "inline-block";
+	document.getElementById('controlsButton').style.display = "none";
+}
+
+function startRead() {
+	var file = document.getElementById('save-file').files[0];
+	if(file){
+		getAsText(file);
+	}
+}
+/*
+$get('http://localhost/foo.txt', function(data) {
+    alert(data);
+});
+*/
+
+function getAsText(readFile) {
+
+  var reader = new FileReader();
+
+  reader.readAsText(readFile, "UTF-16");
+  reader.onerror = errorHandler;
+}
+
+function errorHandler(evt) {
+  if(evt.target.error.name == "NotReadableError") {
+    alert("Something went wrong, please try again!");
+  }
+}
+
+function newGame() {
+	document.getElementById('gameBorder').style.display = "inline-block";
+	document.getElementById('menuBar').style.display = "inline-block";
+	document.getElementById('startButton').style.display = "none";
+	document.getElementById('contButton').style.display = "none";
+	document.getElementById('controlsButton').style.display = "none";
+	
 
 	var slashing = false,
 		slashTimer = false,
 		using = false,
 		menuOpen = false,
-		movementSpeed = 4,
 		stopAnim = false,
+		movementSpeed = 4;
 		ableToMoveUp = true,
 		ableToMoveDown = true,
 		ableToMoveLeft = true,
@@ -41,12 +94,6 @@ function startGame() {
 		slashTime = 5000,
 		indexes = {0:"1", 4:"2", 8:"3", 12:"4", 16:"5", 20:"6", 24:"7", 28:"8", 32:"9", 36:"10", 40:"11", 44:"12", 48:"13", 52:"14", 56:"15", 60:"16", 64:"17", 68:"18", 72:"19", 76:"20", 80:"21", 84:"22", 88:"23", 92:"24", 96:"25", 100:"26", 104:"27", 108:"28", 112:"29", 116:"30", 120:"31", 124:"32", 128:"33", 132:"34", 136:"35", 140:"36", 144:"37", 148:"38", 152:"39", 156:"40", 160:"41", 164:"42", 168:"43", 172:"44", 176:"45", 180:"46", 184:"47", 188:"48", 192:"49", 196:"50", 200:"51", 204:"52", 208:"53", 212:"54", 216:"55", 220:"56", 224:"57", 228:"58", 232:"59", 236:"60", 240:"61", 244:"62", 248:"63", 252:"64", 256:"65", 260:"66", 264:"67", 268:"68", 272:"69", 276:"70", 280:"71", 284:"72", 288:"73", 292:"74", 296:"75", 300:"76", 304:"77", 308:"78", 312:"79", 316:"80", 320:"81", 324:"82", 328:"83", 332:"84", 336:"85", 340:"86", 344:"87", 348:"88", 352:"89", 356:"90", 360:"91", 364:"92", 368:"93", 372:"94", 376:"95", 380:"96", 384:"97", 388:"98", 392:"99", 396:"100", 400:"101", 404:"102", 408:"103", 412:"104", 416:"105", 420:"106", 424:"107", 428:"108", 432:"109", 436:"110"},
 		hitboxes = document.getElementsByClassName("hitbox");
-	for (var i = hitboxes.length - 1; i >= 0; i--) {
-		var currentCheck = hitboxes[i];
-		if (hitboxes[i].style.display === "none") {
-			hitboxes[i].parentNode.removeChild(hitboxes[i])
-		}
-	}
 	var playerHitbox = player.getBoundingClientRect(),
 		hitboxCoords = {left:[], right:[], top:[], bottom:[], left_min:[], left_max:[], right_min:[], right_max:[], top_min:[], top_max:[], bottom_min:[], bottom_max:[]},
 		hblengthDiv = hitboxes.length / 3,
@@ -55,15 +102,8 @@ function startGame() {
 		HBAmount3 = Math.floor(hitboxes.length - hblengthDiv),
 		currentHitbox,
 		loadboxes = document.getElementsByClassName("loadbox");
-	for (var i = hitboxes.length - 1; i >= 0; i--) {
-		var currentCheck = hitboxes[i];
-		if (hitboxes[i].style.display === "none") {
-			hitboxes[i].parentNode.removeChild(hitboxes[i])
-		}
-	}
 	var loadboxCoords = {left:[], right:[], top:[], bottom:[]},
 		LBAmount = 0;
-
 
 	$(document.body).keydown(function (evt) {
 		if(!pressedKeys.includes(evt.keyCode)){
@@ -213,6 +253,30 @@ function startGame() {
 		loadboxCoords.bottom.push(parseInt(loadbox_coords.bottom));
 	}
 
+	$("#saveButton").click(function() {
+		var hearts = 3,
+			locTop = player.style.marginTop,
+			locLeft = player.style.marginLeft,
+			data = { hearts: hearts, location_top: locTop, location_left: locLeft},
+			fileName = "Savegame-" + Math.floor(Date.now() / 1000) + ".savegame";
+		saveData(data, fileName);
+	});
+
+	var saveData = (function () {
+	    var a = document.createElement("a");
+	    document.body.appendChild(a);
+	    a.style = "display: none";
+	    return function (data, fileName) {
+	        var json = JSON.stringify(data),
+	            blob = new Blob([json], {type: "octet/stream"}),
+	            url = window.URL.createObjectURL(blob);
+	        a.href = url;
+	        a.download = fileName;
+	        a.click();
+	        window.URL.revokeObjectURL(url);
+	    };
+	}());
+
 	setInterval(function() {
 		if(pressedKeys[upCode] === true && menuOpen === false) {
 			var top = style.getPropertyValue('margin-top');
@@ -280,7 +344,7 @@ function startGame() {
 		playerHitbox = player.getBoundingClientRect();
 		// Stop moving when hitting a hitbox
 
-		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount] && playerHitbox.left >= hitboxCoords.left[HBAmount] && playerHitbox.right <= hitboxCoords.right[HBAmount]) {
+		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount] && playerHitbox.left >= hitboxCoords.left[HBAmount] && playerHitbox.right <= hitboxCoords.right[HBAmount] && hitboxes[HBAmount].style.display !== "none") {
 			ableToMoveUp = false;
 			currentHitbox = HBAmount;
 			if(pressedKeys[upCode] === true && menuOpen === false && pressedKeys[downCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -292,7 +356,7 @@ function startGame() {
 			ableToMoveUp = true;
 		}
 
-		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount] && playerHitbox.left >= hitboxCoords.left[HBAmount] && playerHitbox.right <= hitboxCoords.right[HBAmount]) {
+		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount] && playerHitbox.left >= hitboxCoords.left[HBAmount] && playerHitbox.right <= hitboxCoords.right[HBAmount] && hitboxes[HBAmount].style.display !== "none") {
 			ableToMoveDown = false;
 			currentHitbox = HBAmount;
 			if(pressedKeys[downCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -304,7 +368,7 @@ function startGame() {
 			ableToMoveDown = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount] && playerHitbox.top <= hitboxCoords.bottom[HBAmount] && playerHitbox.left >= hitboxCoords.right_min[HBAmount] && playerHitbox.left <= hitboxCoords.right_max[HBAmount]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount] && playerHitbox.top <= hitboxCoords.bottom[HBAmount] && playerHitbox.left >= hitboxCoords.right_min[HBAmount] && playerHitbox.left <= hitboxCoords.right_max[HBAmount] && hitboxes[HBAmount].style.display !== "none") {
 			ableToMoveLeft = false;
 			currentHitbox = HBAmount;
 			if(pressedKeys[leftCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[downCode] === false) {
@@ -316,7 +380,7 @@ function startGame() {
 			ableToMoveLeft = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount] && playerHitbox.top <= hitboxCoords.bottom[HBAmount] && playerHitbox.right >= hitboxCoords.left_min[HBAmount] && playerHitbox.right <= hitboxCoords.right_max[HBAmount]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount] && playerHitbox.top <= hitboxCoords.bottom[HBAmount] && playerHitbox.right >= hitboxCoords.left_min[HBAmount] && playerHitbox.right <= hitboxCoords.right_max[HBAmount] && hitboxes[HBAmount].style.display !== "none") {
 			ableToMoveRight = false;
 			currentHitbox = HBAmount;
 			if(pressedKeys[rightCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[leftCode] === false && pressedKeys[downCode] === false) {
@@ -337,7 +401,7 @@ function startGame() {
 		playerHitbox = player.getBoundingClientRect();
 		// Stop moving when hitting a hitbox
 
-		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount2] && playerHitbox.left >= hitboxCoords.left[HBAmount2] && playerHitbox.right <= hitboxCoords.right[HBAmount2]) {
+		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount2] && playerHitbox.left >= hitboxCoords.left[HBAmount2] && playerHitbox.right <= hitboxCoords.right[HBAmount2] && hitboxes[HBAmount2].style.display !== "none") {
 			ableToMoveUp = false;
 			currentHitbox = HBAmount2;
 			if(pressedKeys[upCode] === true && menuOpen === false && pressedKeys[downCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -349,7 +413,7 @@ function startGame() {
 			ableToMoveUp = true;
 		}
 
-		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount2] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount2] && playerHitbox.left >= hitboxCoords.left[HBAmount2] && playerHitbox.right <= hitboxCoords.right[HBAmount2]) {
+		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount2] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount2] && playerHitbox.left >= hitboxCoords.left[HBAmount2] && playerHitbox.right <= hitboxCoords.right[HBAmount2] && hitboxes[HBAmount2].style.display !== "none") {
 			ableToMoveDown = false;
 			currentHitbox = HBAmount2;
 			if(pressedKeys[downCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -361,7 +425,7 @@ function startGame() {
 			ableToMoveDown = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom[HBAmount2] && playerHitbox.left >= hitboxCoords.right_min[HBAmount2] && playerHitbox.left <= hitboxCoords.right_max[HBAmount2]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom[HBAmount2] && playerHitbox.left >= hitboxCoords.right_min[HBAmount2] && playerHitbox.left <= hitboxCoords.right_max[HBAmount2] && hitboxes[HBAmount2].style.display !== "none") {
 			ableToMoveLeft = false;
 			currentHitbox = HBAmount2;
 			if(pressedKeys[leftCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[downCode] === false) {
@@ -373,7 +437,7 @@ function startGame() {
 			ableToMoveLeft = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom[HBAmount2] && playerHitbox.right >= hitboxCoords.left_min[HBAmount2] && playerHitbox.right <= hitboxCoords.right_max[HBAmount2]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount2] && playerHitbox.top <= hitboxCoords.bottom[HBAmount2] && playerHitbox.right >= hitboxCoords.left_min[HBAmount2] && playerHitbox.right <= hitboxCoords.right_max[HBAmount2] && hitboxes[HBAmount2].style.display !== "none") {
 			ableToMoveRight = false;
 			currentHitbox = HBAmount2;
 			if(pressedKeys[rightCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[leftCode] === false && pressedKeys[downCode] === false) {
@@ -394,7 +458,7 @@ function startGame() {
 		playerHitbox = player.getBoundingClientRect();
 		// Stop moving when hitting a hitbox
 
-		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount3] && playerHitbox.left >= hitboxCoords.left[HBAmount3] && playerHitbox.right <= hitboxCoords.right[HBAmount3]) {
+		if (playerHitbox.top >= hitboxCoords.bottom_min[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom_max[HBAmount3] && playerHitbox.left >= hitboxCoords.left[HBAmount3] && playerHitbox.right <= hitboxCoords.right[HBAmount3] && hitboxes[HBAmount3].style.display !== "none") {
 			ableToMoveUp = false;
 			currentHitbox = HBAmount3;
 			if(pressedKeys[upCode] === true && menuOpen === false && pressedKeys[downCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -406,7 +470,7 @@ function startGame() {
 			ableToMoveUp = true;
 		}
 
-		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount3] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount3] && playerHitbox.left >= hitboxCoords.left[HBAmount3] && playerHitbox.right <= hitboxCoords.right[HBAmount3]) {
+		if (playerHitbox.bottom <= hitboxCoords.top_min[HBAmount3] && playerHitbox.bottom >= hitboxCoords.top_max[HBAmount3] && playerHitbox.left >= hitboxCoords.left[HBAmount3] && playerHitbox.right <= hitboxCoords.right[HBAmount3] && hitboxes[HBAmount3].style.display !== "none") {
 			ableToMoveDown = false;
 			currentHitbox = HBAmount3;
 			if(pressedKeys[downCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[leftCode] === false) {
@@ -418,7 +482,7 @@ function startGame() {
 			ableToMoveDown = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom[HBAmount3] && playerHitbox.left >= hitboxCoords.right_min[HBAmount3] && playerHitbox.left <= hitboxCoords.right_max[HBAmount3]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom[HBAmount3] && playerHitbox.left >= hitboxCoords.right_min[HBAmount3] && playerHitbox.left <= hitboxCoords.right_max[HBAmount3] && hitboxes[HBAmount3].style.display !== "none") {
 			ableToMoveLeft = false;
 			currentHitbox = HBAmount3;
 			if(pressedKeys[leftCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[rightCode] === false && pressedKeys[downCode] === false) {
@@ -430,7 +494,7 @@ function startGame() {
 			ableToMoveLeft = true;
 		}
 
-		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom[HBAmount3] && playerHitbox.right >= hitboxCoords.left_min[HBAmount3] && playerHitbox.right <= hitboxCoords.right_max[HBAmount3]) {
+		if (playerHitbox.bottom >= hitboxCoords.top[HBAmount3] && playerHitbox.top <= hitboxCoords.bottom[HBAmount3] && playerHitbox.right >= hitboxCoords.left_min[HBAmount3] && playerHitbox.right <= hitboxCoords.right_max[HBAmount3] && hitboxes[HBAmount3].style.display !== "none") {
 			ableToMoveRight = false;
 			currentHitbox = HBAmount3;
 			if(pressedKeys[rightCode] === true && menuOpen === false && pressedKeys[upCode] === false && pressedKeys[leftCode] === false && pressedKeys[downCode] === false) {
@@ -450,20 +514,19 @@ function startGame() {
 		if (LBAmount >= loadboxes.length) {
 			LBAmount = 0;
 		}
-		console.log(loadboxes[LBAmount].className);
 
 		if ((playerHitbox.top <= loadboxCoords.bottom[LBAmount] && playerHitbox.top >= loadboxCoords.top[LBAmount]) || (playerHitbox.bottom <= loadboxCoords.bottom[LBAmount] && playerHitbox.bottom >= loadboxCoords.top[LBAmount]) || (playerHitbox.top <= loadboxCoords.top[LBAmount] && playerHitbox.bottom >= loadboxCoords.bottom[LBAmount])) {
-			if (playerHitbox.left >= loadboxCoords.left[LBAmount] && playerHitbox.right <= loadboxCoords.right[LBAmount]) {
+			if (playerHitbox.left >= loadboxCoords.left[LBAmount] && playerHitbox.right <= loadboxCoords.right[LBAmount] && loadboxes[LBAmount].style.display === "none") {
 				if (loadboxes[LBAmount].className.includes("ow1Loadbox")) {
-					document.getElementById("overworld_start1").style.display = "none";
-					document.getElementById("overworld_start2").style.display = "block";
+					$("#overworld_start1 *").css("display","none");
+					$("#overworld_start2 *").css("display","");
 					player.style.marginTop = "420px";
-				} else if (loadboxes[LBAmount].className.includes("ow2Loadbox")) {
-					document.getElementById("overworld_start2").style.display = "none";
-					document.getElementById("overworld_start1").style.display = "block";
+				}
+				if (loadboxes[LBAmount].className.includes("ow2Loadbox")) {
+					$("#overworld_start2 *").css("display","none");
+					$("#overworld_start1 *").css("display","");
 					player.style.marginTop = "16px";
 				}
-
 			}
 		}
 
